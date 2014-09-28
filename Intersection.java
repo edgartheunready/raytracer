807495931abec468b94a3e8c29f832c658553b65
Jared Menard <menard.jared@gmail.com>
2014-09-27 22:24:25 -0400

    formatting

--- Intersection.java
+++ Intersection.java
@@ -1,29 +1,29 @@
-public class Intersection {	
-	
-	private double distance;
-	private Point placeHit;
-	private Thing thingHit;
+public class Intersection {
 
-	public Point getPoint() {
-		Point temp = placeHit;
-		return temp;
-	}
+  private double distance;
+  private Point placeHit;
+  private Thing thingHit;
 
-	public Thing getThing() {
-		return thingHit;
-	}
+  public Point getPoint() {
+    Point temp = placeHit;
+    return temp;
+  }
 
-	public Point getIntersectionPoint() {
-		return placeHit;
-	}
+  public Thing getThing() {
+    return thingHit;
+  }
 
-	public double getDistance() {
-		return distance;
-	}
+  public Point getIntersectionPoint() {
+    return placeHit;
+  }
 
-	public Intersection(double distance, Point placeHit, Thing thingHit) {
-		this.distance = distance;
-		this.placeHit = placeHit;
-		this.thingHit = thingHit;
-	}
+  public double getDistance() {
+    return distance;
+  }
+
+  public Intersection(double distance, Point placeHit, Thing thingHit) {
+    this.distance = distance;
+    this.placeHit = placeHit;
+    this.thingHit = thingHit;
+  }
 }
