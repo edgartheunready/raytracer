9ecff86e1f68c1baae38a76e2338e1fc38c31c2e
Jared Menard <menard.jared@gmail.com>
2014-09-27 22:25:42 -0400

    formatting

--- RTLight.java
+++ RTLight.java
@@ -1,21 +1,22 @@
+public class RTLight extends Sphere {
 
-public class RTLight extends Sphere{
-	
-	public RTLight(Point center, double radius,Color sphereColor,Color specularLight){
-		super(center,radius,sphereColor,specularLight);
-	}
-	
-	public Color getAmbient(Point p ){
-		return new Color(0,0,0);
-	}
-	public boolean hasLighting(){
-		return false;
-	}
-	public Color getSpecularWeight(){
-		return new Color(0,0,0);
-	}
-	
-	public Point getCenter(){
-		return center;
-	}
+  public RTLight(Point center, double radius, Color sphereColor, Color specularLight) {
+    super(center, radius, sphereColor, specularLight);
+  }
+
+  public Color getAmbient(Point p) {
+    return new Color(0, 0, 0);
+  }
+
+  public boolean hasLighting() {
+    return false;
+  }
+
+  public Color getSpecularWeight() {
+    return new Color(0, 0, 0);
+  }
+
+  public Point getCenter() {
+    return center;
+  }
 }
--- RTRunnable.java
+++ RTRunnable.java
@@ -1,18 +1,16 @@
+public class RTRunnable implements Runnable {// this is my thread class
+  int startRow, endRow, isFinishedArrayPlace;
+  RayTracer rt;
 
-public class RTRunnable implements Runnable {//this is my thread class
-	int startRow, endRow,isFinishedArrayPlace;
-	RayTracer rt;
-	
-		public RTRunnable(RayTracer rt, int startRow, int endRow,int isFinishedArrayPlace){
-			this.startRow=startRow;
-			this.endRow=endRow;
-			this.rt = rt;
-			this.isFinishedArrayPlace=isFinishedArrayPlace;
-			
-		}
+  public RTRunnable(RayTracer rt, int startRow, int endRow, int isFinishedArrayPlace) {
+    this.startRow = startRow;
+    this.endRow = endRow;
+    this.rt = rt;
+    this.isFinishedArrayPlace = isFinishedArrayPlace;
 
-	    public void run() {
-	        rt.threadPixel( startRow,endRow,isFinishedArrayPlace);	      
-	    }
-	}
+  }
 
+  public void run() {
+    rt.threadPixel(startRow, endRow, isFinishedArrayPlace);
+  }
+}
